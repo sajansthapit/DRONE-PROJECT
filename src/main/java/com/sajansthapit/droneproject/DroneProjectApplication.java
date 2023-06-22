ed45ca257b6e02300959eeaf7d9eda1822806b75
sajansthapit99 <sajan.sthapit@bitskraft.com>
2023-06-22 15:42:13 +0545

    inital project setup

---
+++ src/main/java/com/sajansthapit/droneproject/DroneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sajansthapit.droneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneProjectApplication.class, args);
+	}
+
+}
