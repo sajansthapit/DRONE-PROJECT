922a262adce1997109add0d8894ca9e30d4fc9f5
sajansthapit99 <sajan.sthapit@bitskraft.com>
2023-06-24 22:16:40 +0545

    add: shipment service

---
+++ shipment-service/src/main/java/com/sajansthapit/shipmentservice/ShipmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sajansthapit.shipmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ShipmentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmentServiceApplication.class);
+    }
+}
