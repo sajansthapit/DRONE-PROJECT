c361d1d25804a9751842dc5d8036d59992440c8b
sajansthapit99 <sajan.sthapit@bitskraft.com>
2023-06-24 14:11:45 +0545

    add: medication service module

---
+++ medication-service/src/main/java/com/sajansthapit/medicationservice/MedicationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sajansthapit.medicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicationServiceApplication.class);
+    }
+}
