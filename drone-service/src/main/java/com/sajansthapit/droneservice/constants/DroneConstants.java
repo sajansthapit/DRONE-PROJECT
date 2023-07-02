72a2c3f29ce377622021af314a4abe07acdb39e5
sajansthapit99 <sajan.sthapit@bitskraft.com>
2023-07-02 14:01:56 +0545

    address change

--- drone-service/src/main/java/com/sajansthapit/droneservice/constants/DroneConstants.java
+++ drone-service/src/main/java/com/sajansthapit/droneservice/constants/DroneConstants.java
@@ -2,8 +2,8 @@ package com.sajansthapit.droneservice.constants;
 
 public interface DroneConstants {
     Integer MAX_MEDICATION_WEIGHT = 500;
-    Double CURRENT_LATITUDE = 27.707826;
-    Double CURRENT_LONGITUDE = 85.3104058;
+    Double CURRENT_LATITUDE = 27.706206;
+    Double CURRENT_LONGITUDE = 85.316181;
     Integer TOTAL_MAX_DISTANCE = 10000; //meters
     Integer EARTH_RADIUS = 6371;
 
